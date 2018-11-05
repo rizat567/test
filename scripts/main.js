1bddd3e686e134a781efe33cdd33f64970dfef25
rizat567 <43739426+rizat567@users.noreply.github.com>
2018-11-05 14:19:13 +0600

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/firefox.jpg') {
+      myImage.setAttribute ('src','images/firefox2.jpg');
+	} else {
+	  myImage.setAttribute ('src','images/firefox.jpg');
+	}
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h3');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+    console.log('clicked')
+  setUserName();
+}
